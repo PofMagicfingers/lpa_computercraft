8cd0c7a4ed3b90b9223708b184b29644c75039b0
PofMagicfingers <pof@pof.pm>
2014-10-18 05:13:16 +0200

    Update and rename lpa to lpa.lua
    Support spec files, and remove lua extension at download

--- lpa.lua
+++ lpa.lua
@@ -19,12 +19,12 @@ local function printUsage()
 	print( "by: Pof Magicfingers" )
 	print( " " )
 	print( "Utilisation:" )
-	print( " " )
 	print( " == Recuperation ==" )
 	print( "  lpa (episode) (program)" )
-	print( " " )
 	print( " == Liste des programmes ==" )
 	print( "  lpa (episode)" )
+	print( " == Mise a jour LPA ==" )
+	print( "  lpa update" )
 	print( " " )
 end
 
@@ -35,9 +35,15 @@ function decode(resp)
 
   i, j = string.find(resp, "/PofMagicfingers/lpa_computercraft/blob/[^/]+/")
   while i ~= nil do
-    table[index] = string.sub(resp, j+1, string.find(resp, "\"", j)-1)
+  	content = string.sub(resp, j+1, string.find(resp, "\"", j)-1)
+  	if not string.match(content, ".spec$") and not string.match(content, ".md$") then
+  		if string.match(content, ".lua$") then
+  			content = string.gsub(content, ".lua$","")
+  		end
+	    table[index] = content
+	    index = index + 1
+	end
     i, j = string.find(resp, "/PofMagicfingers/lpa_computercraft/blob/[^/]+/", j)
-    index = index + 1
   end
 
   return table
@@ -78,15 +84,22 @@ end
 local function getFile(episode, program)
 	termClear()
 
-    local sPath = shell.resolve( program )
-    if episode == "" then
-		print( "Vous devez fournir un numero d'episode !" )
-		return
+	if episode == "update" and program == "" then
+		episode = "master" ; program = "lpa"
 	end
+
+    if episode == "" then
+		print( "Vous devez fournir un numero d'episode !" ) ; return
+	else
+		if type(tonumber(episode)) == "number" then
+			episode = "ep"..tostring(tonumber(episode))
+		end
+	end
+
 	if program == "" then
 		termClear()
 		write( "Connexion... " )
-		local response = http.get("https://github.com/PofMagicfingers/lpa_computercraft/file-list/ep"..episode)
+		local response = http.get("https://github.com/PofMagicfingers/lpa_computercraft/file-list/"..episode)
 		if response then
 			local sResponse = response.readAll()
 			response.close()
@@ -102,29 +115,60 @@ local function getFile(episode, program)
 		end
 		return
 	end
-	if fs.exists( sPath ) then
-		local override = ""
-		printed = 0
-		while override ~= "n" and override ~= "o" do
-			if printed == 3 then 
-				termClear()
-				printed = 0
-			end
-			print( "Le programme "..program.." existe !" )
-			print( "Remplacer ? (o pour oui, n pour non)" )
-			print( " " )
-			event, override = os.pullEvent ("char")
-			printed = printed + 1
-		end
-		if override == "n" then return end
-	end
 	termClear()
 	write( "Connexion... " )
-	local response = http.get("https://raw2.github.com/PofMagicfingers/lpa_computercraft/ep"..episode.."/"..program)
+	local response = http.get("https://raw2.github.com/PofMagicfingers/lpa_computercraft/"..episode.."/"..program..".lua")
+	if not response then
+		response = http.get("https://raw2.github.com/PofMagicfingers/lpa_computercraft/"..episode.."/"..program)
+	end
 	if response then
 		print( "OK !" )
 		local sResponse = response.readAll()
 		response.close()
+		
+	    local sPath = shell.resolve( program )
+
+		local spec_response = http.get("https://raw2.github.com/PofMagicfingers/lpa_computercraft/"..episode.."/"..program..".spec")
+		if spec_response then
+			local tResponse = textutils.unserialize(spec_response.readAll())
+			spec_response.close()
+			if tResponse and tResponse["installPath"] then
+				sPath = tResponse["installPath"] ; program = sPath
+			end
+		end
+
+		sDirPath = string.match(sPath, "(.-)([^/]-)$")
+
+		pcall(function() fs.makeDir(sDirPath) end)
+
+		if (not fs.isDir( sDirPath ) and fs.exists( sDirPath )) then
+			print("Impossible de creer le dossier d'installation "..sDirPath)
+			print("Creez le dossier manuellement et reessayez.")
+			return
+		end
+
+		if fs.exists( sPath ) then
+			if fs.isDir( sPath ) then
+				print("Un dossier "..sPath.." existe deja, renommez le ou supprimez le pour pouvoir installer ce programme")
+				return
+			else
+				local override = ""
+				printed = 0
+				while override ~= "n" and override ~= "o" do
+					if printed == 3 then 
+						termClear()
+						printed = 0
+					end
+					print( "Le programme "..program.." existe !" )
+					print( "Remplacer ? (o pour oui, n pour non)" )
+					print( " " )
+					event, override = os.pullEvent ("char")
+					printed = printed + 1
+				end
+				if override == "n" then return end
+			end
+		end
+		
 		local file = fs.open( sPath, "w" )
 		file.write( sResponse )
 		file.close()
@@ -146,11 +190,11 @@ local function getFile(episode, program)
 	end
 end
 
-    local gui_mode = false
-    if #tArgs < 1 then
-		printUsage()
-		return
-	end
-    local episode = tArgs[1] or ""
-	local program = tArgs[2] or ""
-    getFile(episode, program)
+local gui_mode = false
+if #tArgs < 1 then
+	printUsage()
+	return
+end
+local episode = tArgs[1] or ""
+local program = tArgs[2] or ""
+getFile(episode, program)
